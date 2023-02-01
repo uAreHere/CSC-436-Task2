65bce669a4a7ad2f3d0e8624850e94ee845006c8
e_flores <83792705+uAreHere@users.noreply.github.com>
2023-02-01 18:29:48 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import {eventHandler} from './utils/event';
+import { multiplyEvent } from './utils/multiplyer';
+
+document.querySelector('#app').innerHTML = `
+<div class="container my-5">
+<div class="mb-3">
+      <label for="number-1" class="form-label">First Number</label>
+      <input type="number" class="form-control" id="number-1">
+</div>
+<div class="mb-3">
+      <label for="number-2" class="form-label">Second Number</label>
+      <input type="number" class="form-control" id="number-2">
+</div>
+<button id="add" class="btn btn-primary mb-3">Add</button>
+<button id="multiply" class="btn btn-primary mb-3">Multiply</button>
+<div class="mt-5 rounded border p-5 bg-secondary text-white">
+      Result: <div id="result">
+</div>
+</div>
+`;
+eventHandler();
+multiplyEvent();
+
